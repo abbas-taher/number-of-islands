1222ea9a3238df778364f7083c2ea93a6aee45f4
Abbas Taher <abbtah@yahoo.com>
2020-12-14 10:59:16 -0500

    Update number-of-islands.py

--- number-of-islands.py
+++ number-of-islands.py
@@ -28,18 +28,25 @@ Constraints:
     1 <= m, n <= 300
     grid[i][j] is '0' or '1'.
  """
+
+# The proposed Solution is quite simple and does not use recurssion.
+# Simply we traverse the grid line by line and item by item to check for a given item if
+# the preceeding item or the one above belong to an island - if so then add it to that island
+
  class Grid(object):
     def __init__(self, mat):
         self.mat = mat
-        self.islandLst =[]
-        
-    def isBefore(self, i,j):
+        self.islandLst =[]     # list of dictionaries (islands)
+    
+    # for a given (i,j) check that preceeding item belongs to an island; if so add it to that island
+    def isBefore(self, i,j):      
         for island in self.islandLst:
             if j != 0 and (i,j-1) in island:
                 island[(i,j)] = None
                 return (True) 
         return (False)
     
+    # for a given (i,j) check that above item belongs to an island; if so add it to that island
     def isAbove(self, i,j):
         for island in self.islandLst:
             if i != 0 and (i-1,j) in island:
@@ -53,7 +60,7 @@ Constraints:
             for j in range (len(self.mat[0])):
              if self.mat[i][j] == '1':
                  if not self.isBefore(i,j) and not self.isAbove(i,j):
-                     self.islandLst.append({(i,j):None})
+                     self.islandLst.append({(i,j):None})                # if item does not belong to any island then create an island for it  
                      
         return (len(self.islandLst))
     
